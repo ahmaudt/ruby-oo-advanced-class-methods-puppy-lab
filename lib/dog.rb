0616fde8c0fee49453272b0f0e851c76e3ab7712
Ahmaud R. Templeton <ahmaud@gmail.com>
2021-02-05 16:31:00 -0500

    commit tree 68110dd175d275d5904246086b46b6bc14ccbac5

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,33 @@
 # Add your code here
+require 'pry'
+
+class Dog
+
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        self.save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+       @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+    def self.print_all
+        doggies = @@all.find { |doggy| puts doggy.name }
+    end
+
+end
+
+snoopy = Dog.new("Snoopy")
